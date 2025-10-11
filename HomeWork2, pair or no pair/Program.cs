da9ae2f7c219365da7b11ae1c53e093889c347c1
Debbie :p <debora121206@gmail.com>
2025-10-11 16:53:55 -0400

    pair or no pair
    Subiendo tarea 2, par o impar

---
+++ HomeWork2, pair or no pair/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Pair_or_no_pair
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool finish = false;
+            do
+            {
+                Console.WriteLine("insert one number:");
+
+                try
+                {
+                    int number = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine($"your number is {number}");
+                    if (number % 2 == 0)
+                    {
+                        Console.WriteLine("Is Pair");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not is Pair");
+                    }
+                    finish = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("You can't insert other value, except numbers");
+                    Console.WriteLine("Press one key for continue...");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            } while (!finish);
+        }
+    }
+}
